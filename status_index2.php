532ad5e0f7207b5ce36f2906b20760aba263e82c
varun324 <varunwadhwani324@gmail.com>
2015-05-13 16:42:01 +0530

    status check

---
+++ status_index2.php
@@ -0,0 +1,32 @@
+<?php
+include 'connect.php';
+include 'core.php';
+if(isset($_POST['reference_no']))
+{
+  //echo "asd";
+  $reference_no=$_POST['reference_no'];
+  if(!empty($reference_no))
+  {
+      //echo 'success'; 
+
+
+       $sql="SELECT status FROM `complaint_registration` WHERE `reference_no`='$reference_no'";
+      //$query=mysql_query($sql);
+
+
+     
+     if($result=mysql_query($sql))
+     {
+        while ($row=mysql_fetch_array($result))
+        {
+          printf ("status of your complaint is : %s\n",$row["status"]);
+
+          //echo "<script>alert(".$row['status'].")</script>";
+        }
+        // Free result set
+         // mysqli_free_result($result);
+      }
+  }
+}
+
+?>
